a939784b429d041b79d3413a3786476407568d3f
Igor.R <ri.main@ya.ru>
2019-05-26 19:55:41 +0700

    +devServer

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const config = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                use: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    },
+    resolve: {
+        extensions: [
+            '.js',
+            '.jsx'
+        ]
+    },
+    devServer: {
+        historyApiFallback: true,
+        watchOptions: {aggregateTimeout: 300, pool: 1000},
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+            "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+        },
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8080'
+            },
+        }
+    }
+}
+
+module.exports = config;
